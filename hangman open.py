5969dd142c41578aa14fe01e7b75076f02ea57d0
avivellen14 <118472198+avivellen14@users.noreply.github.com>
2025-01-11 16:35:36 +0200

    Update hangman open.py

--- hangman open.py
+++ hangman open.py
@@ -11,9 +11,18 @@ HANGMAN_ASCII_ART = """
 MAX_TRIES = 6
 print ("Welcome to the game Hangman" , HANGMAN_ASCII_ART , "\n" , MAX_TRIES)
 
-guessed_letter = input ("Guess a letter: ")
-guessed_letter = guessed_letter.lower()
-print (guessed_letter)
+guessed_letter = str(input ("Guess a letter: "))
+if guessed_letter.isalpha() and len(guessed_letter) >1:
+    print ("E1")
+elif not guessed_letter.isalpha() and len(guessed_letter) == 1:
+    print ("E2")
+elif not guessed_letter.isalpha() and len(guessed_letter) > 1:
+    print ("E3")
+elif guessed_letter.isalpha() and len(guessed_letter) == 1:
+    print (guessed_letter.lower())
+else:
+    print ("oops ")
+
 
 guessed_word = input("Please enter a string: ")
 guessed_word_length = len (guessed_word) 
@@ -21,3 +30,7 @@ first_part_word = guessed_word[:guessed_word_length//2].lower()
 second_part_word = guessed_word[guessed_word_length//2 : ].upper()
 new_word =  first_part_word + second_part_word
 print (new_word)
+
+guessing = input("Please enter a word: ")
+guessing_length = len(guessing)
+print ("_ " * guessing_length)
